c5b9fec22b30f5d3a3a9dc3ebef97babb518e48c
Niklas Trzaska <niklastrzaska@MacBook-Air-von-Niklas.local>
2022-07-13 22:55:10 +0200

    Debugging

--- lib/basic_pipeline-stack.ts
+++ lib/basic_pipeline-stack.ts
@@ -1,4 +1,4 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { pipelines, SecretValue, Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { BasicPipeline } from './constructs/BasicPipeline';
 
@@ -8,9 +8,15 @@ export class BasicPipelineStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    new BasicPipeline(this, 'myBasicPipeline', {pipelineName: 'basicPipeline'})
+   // new BasicPipeline(this, 'myBasicPipeline', {pipelineName: 'basicPipeline'})
 
-
+   const pipeline = new pipelines.CodePipeline(this, 'Pipeline', {
+    pipelineName: 'MyPipeline',
+    synth: new pipelines.ShellStep('Synth', {
+        input: pipelines.CodePipelineSource.gitHub('Sleep298/basicPipeline', 'main', {authentication: SecretValue.secretsManager("GitHub-token")}),
+        commands: ['npm ci', 'npm run build', 'npx cdk synth']
+    })     
+});
 
     
   }
