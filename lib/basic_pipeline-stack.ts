e2f2bee0f255fdc91bed02746a33a37c3b4b8a70
Niklas Trzaska <niklastrzaska@MacBook-Air-von-Niklas.local>
2022-07-13 22:29:33 +0200

    debuging: copy minimal example

--- lib/basic_pipeline-stack.ts
+++ lib/basic_pipeline-stack.ts
@@ -1,12 +1,23 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 import { Construct } from 'constructs';
-import { BasicPipeline } from "./constructs/BasicPipeline";
+import { BasicPipeline } from "./BasicPipeline";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class BasicPipelineStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    new BasicPipeline(this, 'myBasicPipeline', {pipelineName: 'basicPipeline'})
+    //new BasicPipeline(this, 'myBasicPipeline', {pipelineName: 'basicPipeline'})
+
+    const pipeline = new CodePipeline(this, 'Pipeline', {
+      pipelineName: 'MyPipeline',
+      synth: new ShellStep('Synth', {
+        input: CodePipelineSource.gitHub('Sleep298/basicPipeline', 'main'),
+        commands: ['npm ci', 'npm run build', 'npx cdk synth']
+      })
+    });
+
+    
   }
 }
