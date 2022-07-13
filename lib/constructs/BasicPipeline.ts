6414f22ca59c770483d60d5f6d068e5824350c54
Niklas Trzaska <niklastrzaska@MacBook-Air-von-Niklas.local>
2022-07-13 22:12:08 +0200

    debugging: prop pipelineName source?

--- lib/constructs/BasicPipeline.ts
+++ lib/constructs/BasicPipeline.ts
@@ -13,8 +13,8 @@ export class BasicPipeline extends Construct {
     constructor(scope: Construct, id: string, props: IBasicPipeline) {
         super(scope, id);
     
-        this.basicPipeline = new pipelines.CodePipeline(this, props.pipelineName, {
-            pipelineName: props.pipelineName,
+        this.basicPipeline = new pipelines.CodePipeline(this, 'Pipeline', {
+            pipelineName: 'MyPipeline',
             synth: new pipelines.ShellStep('Synth', {
                 input: pipelines.CodePipelineSource.gitHub('Sleep298/basicPipeline', 'main'),
                 commands: ['npm ci', 'npm run build', 'npx cdk synth']
